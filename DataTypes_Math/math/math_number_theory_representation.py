c1f8ab45dacc09064d13d92ba456f30b1b1ca254
André Luiz Albano <110417140+albano-a@users.noreply.github.com>
2025-06-22 18:16:14 -0300

    New formatting for each file

--- DataTypes_Math/math/math_number_theory_representation.py
+++ DataTypes_Math/math/math_number_theory_representation.py
@@ -1,28 +1,53 @@
 import math
 
-x = 8.0
-mantissa, exponent = math.frexp(x)
+"""
+comb(n, k)
+factorial(n)
+gcd(*integers)
+isqrt(n)
+lcm(*integers)
+perm(n, k)
+"""
 
-m, e = 0.5, 4
-ld = math.ldexp(m, e)
+# Number of ways to choose k items from n items without repetitions and without order
+n = 6
+k = 2
 
-y = 8.495
-modf_parts = math.modf(y)
+res1 = math.comb(n, k)
+print(f"The number of ways to choose {k} items from {n} items is {res1}")
 
-print(
-    math.ceil(2.3),         # round up
-    math.floor(2.9),        # round down
-    math.trunc(-2.7),       # truncate toward 0
+# Number of ways to choose k items from n items without repetition and with order
+res1_1 = math.perm(n, k)
+print(f"The number of ways to choose {k} items from {n} items with order is {res1_1}")
 
-    math.copysign(3, -2),   # copy sign
-    math.fabs(-5),          # absolute (float)
-    math.factorial(5),      # factorial
 
-    math.fmod(5.5, 2.2),    # float modulo
+# n factorial
 
-    (mantissa, exponent),   # frexp decomposition
-    mantissa * 2**exponent, # frexp reconstruction
+a1 = 5
+a2 = 6
+a3 = 7
 
-    ld,                     # ldexp result
-    modf_parts              # modf (fractional, integer)
-)
+res2 = math.factorial(a1)
+res3 = math.factorial(a2)
+res4 = math.factorial(a3)
+
+print(f" {a1}! = {res2}; {a2}! = {res3}; {a3}! = {res4}")
+
+# Greatest common divisor of the integer arguments
+
+a, b, c = 4, 8, 12
+
+res5 = math.gcd(a, b, c)
+print(f"The GDC of {a}, {b}, {c} is {res5}")
+
+# Least common multiple of the integer arguments
+res6 = math.lcm(a, b, c)
+print(f"The LCM of {a}, {b}, {c} is {res6}")
+
+# Integer square root of a nonnegative integer n
+
+n = 4096
+
+res7 = math.isqrt(n)
+print(f"The integer square root of {n} is {res7}")
+
